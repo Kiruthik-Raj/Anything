7f91585ade999b7a51eddeb6829234bd0189ff3c
Kiruthik-Raj <gmkiruthick02@gmail.com>
2024-05-29 23:02:06 +0530

    Updated json files

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ const Search = ({ handleSearchNote }) =>
 {
     return <div className='search'>
         <MdSearch className = 'search-icons' size='1.3em' / >
-        <input onChange = {(event) => handleSearchNote(event.target.value)} 
+        <input onChange = {(event) => handleSearchNote(event.target.value)}
         type = "text" placeholder="Type to find notes..."></input>
     </div>;
 };
